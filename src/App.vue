d346fe1b92e5c2e053557d4131fde90344ec3646
codedbychavez <codedbychavez@gmail.com>
2022-07-31 10:33:32 -0300

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,50 @@
 <script>
-import Table from './components/Table.vue';
-import TableRow from './components/TableRow.vue';
-import TableData from './components/TableData.vue';
-import data from './data'
+import Table from "./components/Table.vue";
+import TableRow from "./components/TableRow.vue";
+import TableData from "./components/TableData.vue";
+import data from "./data";
 
 export default {
   data() {
     return {
-      headers: ['ID', 'First Name', 'Last Name', 'Resume'],
+      headers: ["ID", "First Name", "Last Name", "Resume"],
       users: data,
       file: null,
-      tableRow: null,
       userId: "",
-    }
+    };
   },
   components: {
     Table,
     TableRow,
-    TableData
+    TableData,
   },
   methods: {
     reset() {
-      if(this.file) {
+      if (this.file) {
         this.file = null;
         this.$refs[`file${this.userId}`][0].innerHTML = "";
-        this.$refs[`tableRow${this.userId}`][0].$el.classList.remove("bg-green-300");
+        this.$refs[`tableRow${this.userId}`][0].$el.classList.remove(
+          "bg-green-300"
+        );
       }
     },
-    dragOver(userId, event) {
+    addRowBackground(userId) {
+      if (userId === undefined) {
+        userId = this.userId;
+      }
       this.$refs[`tableRow${userId}`][0].$el.classList.add("bg-green-300");
     },
-    dragLeave(userId, event) {
+    removeRowBackground(userId) {
+      if (userId === undefined) {
+        userId = this.userId;
+      }
       this.$refs[`tableRow${userId}`][0].$el.classList.remove("bg-green-300");
+    },
+    dragOver(userId, event) {
+      this.addRowBackground(userId);
+    },
+    dragLeave(userId, event) {
+      this.removeRowBackground(userId);
     },
     drag(e) {
       // console.log('drag')
@@ -39,38 +52,55 @@ export default {
     drop(userId, event) {
       this.reset();
       this.userId = userId;
-      this.tableRow = event.currentTarget;
       this.file = event.dataTransfer.files[0];
       this.afterFileSelection();
     },
-    doubleClick(e) {
-      // console.log('double click')
-      // TODO: 
+    doubleClick(userId, event) {
+      this.reset();
+      this.userId = userId;
+      this.addRowBackground();
+      let input = document.createElement("input");
+      input.type = "file";
+      input.onchange = () => {
+        let files = Array.from(input.files);
+        this.file = files[0];
+        console.log(this.file.name);
+        this.afterFileSelection();
+      };
+      input.click();
     },
 
     afterFileSelection() {
       // Show file details
       this.$refs[`file${this.userId}`][0].innerHTML = this.file.name;
-      // this.$refs[`fileInput${this.currentIndex}`][this.currentIndex] = this.file;
-    }
+    },
+
+    handleFileUpload() {
+      const formData = new FormData();
+      formData.append("file", this.file);
+      console.log("Uploading...", this.file.name);
+      // Do your fetch here
+    },
   },
-}
+};
 </script>
 
 <template>
   <div class="p-4">
     <Table :headers="headers">
-      <div class="table-row-wrapper w-full"
-        v-for="user in users" :key="user.id"
+      <div
+        class="table-row-wrapper w-full"
+        v-for="user in users"
+        :key="user.id"
       >
-
-        <TableRow :ref="`tableRow${user.id}`"
+        <TableRow
+          :ref="`tableRow${user.id}`"
           @dragover.prevent="dragOver(user.id, $event)"
           @dragleave.prevent="dragLeave(user.id, $event)"
           @drag.prevent="drag"
           @drop.prevent="drop(user.id, $event)"
-          @dblclick.prevent="doubleClick" 
-          >
+          @dblclick.prevent.stop="doubleClick(user.id, $event)"
+        >
           <TableData>
             {{ user.id }}
           </TableData>
@@ -80,20 +110,19 @@ export default {
           <TableData>
             {{ user.lastName }}
           </TableData>
-          <TableData>         
+          <TableData>
             <!-- File input may go here -->
             <button class="bg-gray-200 px-2 py-1 rounded-md">Upload</button>
             <div :ref="`file${user.id}`" class="ml-2"></div>
           </TableData>
         </TableRow>
       </div>
-
     </Table>
   </div>
 </template>
 
 <style scoped>
-  .table-row-wrapper:nth-child(even) {
-    background-color: rgba(243, 244, 246);
-  }
+.table-row-wrapper:nth-child(even) {
+  background-color: rgba(243, 244, 246);
+}
 </style>
